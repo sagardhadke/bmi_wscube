58e0bd43397dd65d8743bfe277cad6d47484037b
Sagar Dhadke <peaceunique2003@gmail.com>
2025-03-26 11:47:45 +0530

    Remove Unwanted Code

---
+++ lib/bmiScreen.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class MyBMI extends StatelessWidget {
+  const MyBMI({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          
+        ],
+      ),
+    );
+  }
+}
